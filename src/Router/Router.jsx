774fbd8968279dc839c74606b0dee3d639eabd3f
sharifulislamudoy <surifroton301@gmail.com>
2025-05-07 18:02:00 +0600

    fix simple bugs

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -112,7 +112,9 @@ export const router = createBrowserRouter([
       },
       {
         path:'/dashboard',
-        element:<Dashboard></Dashboard>
+        element:<PrivateRoute>
+          <Dashboard></Dashboard>
+        </PrivateRoute>
       }
     ]
   },
